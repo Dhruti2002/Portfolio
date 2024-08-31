2b490b7e4ef19ade852691513f07495cf9ae64b9
Dhruti2002 <dhrutipatel1120@gmail.com>
2024-08-31 12:24:27 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 
 import Navbar from './components/Navbar';
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/contact/Contact'
+import Themes from './components/Themes';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/contact/Contact';
 
 
 function App() {
   return (
     <BrowserRouter >
       <Navbar />
+      <Themes />
       <Routes>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
